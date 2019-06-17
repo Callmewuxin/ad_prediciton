3a177c7f5d67975ce2914445dd90affd50e38646
Callmewuxin <13917014482@163.com>
2019-06-17 17:52:18 +0800

    first commit

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,11 @@
+Detecting Alzheimer’s Disease from T1-weighted MRI Coronal Brain Scans 
+
+Shakthi Visagan
+
+Dr. Aaron Meyer
+
+Bioengr 188: Machine learning & data-driven modeling in bioengineering
+
+Final Project
+
+15 March 2018
---
+++ test.py
@@ -0,0 +1,64 @@
+import sklearn
+import cv2
+import os
+import numpy as np
+from flask import Flask, request
+import flask
+import tensorflow as tf
+from keras.models import load_model
+from sklearn import cluster
+
+
+def loadmodel():
+    print('start load model')
+    global model
+    model_dir = 'ad_prediction.h5' # model path
+    if os.path.exists(model_dir):
+        print('yes it is')
+        model = load_model(model_dir)
+    else:
+        print('it doesnt')
+    global graph
+    graph = tf.get_default_graph()
+
+
+def prepPNGimgs(array_of_image_paths):
+    l = []
+    for img_file in array_of_image_paths:  # for each file in the list of images...
+        img = cv2.imread("{}".format(img_file))  # read the image...
+        img = np.array(img, dtype=np.float64) / 255
+        w, h, d = original_shape = tuple(img.shape)
+        assert d == 3
+        image_array = np.reshape(img, (w * h, d))
+        kmeans = sklearn.cluster.KMeans(n_clusters=2, random_state=0).fit(image_array)
+        labels = kmeans.predict(image_array)
+
+        def recreate_image(codebook, labels, w, h):
+            """Recreate the (compressed) image from the code book & labels"""
+            d = codebook.shape[1]
+            image = np.zeros((w, h, d))
+            label_idx = 0
+            for i in range(w):
+                for j in range(h):
+                    image[i][j] = (codebook[labels[label_idx]])
+                    label_idx += 1
+            return image
+
+        clustImg = recreate_image(kmeans.cluster_centers_, labels, w, h)
+
+        l.append(clustImg)
+
+    return (np.asarray(l))
+
+
+
+loadmodel()
+imglist = []
+img = cv2.imread()
+imgname = img.filename
+imglist.append(imgname)
+X = prepPNGimgs(imglist)
+if X[0][1] == 1:
+    print("Alzheimer’s Disease(AD)")
+else:
+    print("No Condition(NC)")
